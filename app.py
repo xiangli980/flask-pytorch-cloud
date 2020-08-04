602648787697dcf66ac35671a58c5add093ce6a7
xiangli980 <46658917+xiangli980@users.noreply.github.com>
2020-08-04 15:25:40 -0400

    Add files via upload

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+from flask import render_template
+from datetime import datetime
+from inference import test_sample,creat_model
+from math import sqrt
+from time import sleep
+import json
+import cv2
+
+app = Flask(__name__)
+
+@app.route("/",methods=['GET', 'POST'])
+
+def home():
+    if request.method == "POST":
+        path = request.json["name"]
+        print(path)
+        #fileid = path.split(".")[0].split("_")[1]
+        model = creat_model()
+        msk = test_sample(model)
+        print(msk.shape)
+        #regs = get_json(fileid)
+        #with open('./static/{}.json'.format(fileid), 'w') as fp:
+        #   json.dump(regs, fp)
+        cv2.imwrite("./output.png", msk[:,:,None].repeat(3,axis=2))
+        return {"msk":msk.tolist()}
+        
+    return render_template("new.html")
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    # Flask's development server will automatically serve static files in
+    # the "static" directory. See:
+    # http://flask.pocoo.org/docs/1.0/quickstart/#static-files. Once deployed,
+    # App Engine itself will serve those files as configured in app.yaml.
+    app.run(host='127.0.0.1', port=5000, debug=True)
